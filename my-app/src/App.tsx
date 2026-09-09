1f869cc7d81484f318ab06b115e8bf1173367aaa
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-09 09:22:24 -0300

    feat: Estrutura de rotas no main. Teste em URL direta.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,11 @@
-import { Outlet } from 'react-router'; // 1. Importe o Outlet
+import { Outlet } from 'react-router';
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-// 2. Apague a importação do Conteudo
 import Rodape from "./components/Rodape/Rodape";
 
 export default function App() {
   return (
     <div>
       <Cabecalho />
-      {/* 3. Troque o <Conteudo /> pelo <Outlet /> */}
       <Outlet /> 
       <Rodape/>
     </div>
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,15 +1,25 @@
-import { Outlet } from 'react-router'; // 1. Importe o Outlet
-import Cabecalho from "./components/Cabecalho/Cabecalho";
-// 2. Apague a importação do Conteudo
-import Rodape from "./components/Rodape/Rodape";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 
-export default function App() {
-  return (
-    <div>
-      <Cabecalho />
-      {/* 3. Troque o <Conteudo /> pelo <Outlet /> */}
-      <Outlet /> 
-      <Rodape/>
-    </div>
-  )
-}
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from './routes/Home/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />, errorElement: <Error />, children: [
+    {path: '/', element: <Home />},
+    {path: '/produtos', element: <Produtos />},
+    {path: '/editar-produtos', element: <EditarProdutos />}
+
+]},
+
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
