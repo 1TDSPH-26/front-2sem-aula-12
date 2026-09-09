2d02193e5f82f463407b8d1ea6bd28f19e8b5919
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-09-09 08:51:57 -0300

    feat: Implementação do react router no projeto, e no main tsx adicionando as principais rotas

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Error from './routes/Error/index.tsx'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Prodoutos/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />, errorElement: <Error/>,children:[
+    {path:"/", element:<Home/>},
+    {path:"/produtos", element:<Produtos/>},
+    {path:"/editar-produtos", element:<EditarProdutos/>},
+  ]},
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
