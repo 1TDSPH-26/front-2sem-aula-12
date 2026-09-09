5207492debd4e569ba3842df046f377b19a995ec
gabefeitosa <gabrielfeitosa321@outlook.com>
2026-09-09 09:42:23 -0300

    feat: criado barra de navegação com as rotas

--- my-app/src/componentes/Cabecalho/Cabecalho.tsx
+++ my-app/src/componentes/Cabecalho/Cabecalho.tsx
@@ -1,3 +1,5 @@
+import Menu from "../Menu/Menu";
+
 export default function Cabecalho(){
 
     return(
@@ -6,14 +8,7 @@ export default function Cabecalho(){
                 <h1>
                     Cabeçalho
                 </h1>
-                <nav>
-                    <ul>
-                        <li>Item-1</li>
-                        <li>Item-2</li>
-                        <li>Item-3</li>
-
-                    </ul>
-                </nav>
+                <Menu/>
             </header>
         </div>
     );
