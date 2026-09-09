1ac44b1988a09beb6031a83450460fd1da0b4220
Luigi Tormim <luiginhotoca@gmail.com>
2026-09-09 09:42:23 -0300

    feat : criando nav no componente menu, e as rotas das novas paginas, home, editarproduto, produto e error

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Home/Produtos/index.tsx'
+import EditarProdutos from './routes/Home/EditarProdutos/index.tsx'
+import Error from './routes/Home/Error/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/produtos',
+    element: <Produtos />
+  },
+  {
+    path: '/editar-produtos',
+    element: <EditarProdutos />
+  },
+  {
+    path: '*',
+    element: <Error />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
