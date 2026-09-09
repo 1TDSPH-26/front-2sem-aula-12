7cc0b3a2949fc13a61bd2dd7e70677c5dedbcfbf
carlostsucamoto <caduchiarelli@hotmail.com>
2026-09-09 09:26:35 -0300

    fix: atualização da main

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,23 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import { createBrowserRouter, Router, RouterProvider } from 'react-router'
-import Home from "./routes/Home/index.tsx"
-import Produtos from "./routes/Produtos/index.tsx"
-import EditarProdutos from "./routes/EditarProdutos/index.tsx"
-import Error from "./routes/Error/index.tsx"
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from './routes/Home/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
 
 const router = createBrowserRouter([
   {path:'/', element: <App/>, errorElement: <Error/>, children:[
-    {path: '/', element: <Home />},
-    {path: '/produtos', element: <Produtos/>},
-    {path: '/editar-produtos', element: <EditarProdutos/>},
-    {path: '/', element: <Error/>}
+    {path: '/', element: <Home/>},
+    {path:'/produtos',element:<Produtos/>},
+    {path:'/editar-produtos', element:<EditarProdutos/>},
   ]},
+
 ]);
 
 createRoot(document.getElementById('root')!).render(
-<StrictMode>
-  <RouterProvider router={router} />
-</StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
