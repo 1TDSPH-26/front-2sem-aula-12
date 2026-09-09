ea7b1465a0e5a7d51bde19c834ebbaed4f6007f3
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-09 08:56:58 -0300

    feat: concerta conflitos

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,46 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
+import { Outlet } from 'react-router'; // 1. Importe o Outlet
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+// 2. Apague a importação do Conteudo
+import Rodape from "./components/Rodape/Rodape";
 
-// Passo 5: Importando as ferramentas do React Router
-import { createBrowserRouter, RouterProvider } from 'react-router'
-
-import App from './App.tsx'
-
-// Passo 4: Importando as telas
-import Home from './routes/Home'
-import Produtos from './routes/Produtos'
-import EditarProdutos from './routes/EditarProdutos'
-import Error from './routes/Error'
-
-// Passo 6: Criando o mapa (lista) de rotas
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />, // Essa tela aparece se o usuário digitar um endereço que não existe
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/produtos", element: <Produtos /> },
-      { path: "/editar-produtos", element: <EditarProdutos /> }
-    ]
-  }
-]);
-
-// Passo 7: Ligando o roteador na aplicação
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
-=======
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-
->>>>>>> 61c27dd30471a42e29beceb8e9d990298b060df3
+export default function App() {
+  return (
+    <div>
+      <Cabecalho />
+      {/* 3. Troque o <Conteudo /> pelo <Outlet /> */}
+      <Outlet /> 
+      <Rodape/>
+    </div>
+  )
+}
